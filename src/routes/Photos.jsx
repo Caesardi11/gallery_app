63cebb86e197bda36bec2a16b61ed3fc736cda75
Caesardi11 <ardicaesarkurniawan@gmail.com>
2022-12-16 23:50:59 +0700

    commit pertama

---
+++ src/routes/Photos.jsx
@@ -0,0 +1,113 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Card from '../components/Card';
+
+const Photos = () => {
+	const [photos, setPhotos] = useState([]);
+	const [sort, setSort] = useState('asc');
+	const [submited, setSubmited] = useState('');
+	const [search, setSearch] = useState('');
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	const deletePhoto = (id) => {
+		fetch(`https://gallery-app-server.vercel.app/photos/${id}`, {
+			method: 'DELETE',
+		})
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setPhotos(photos.filter((photo) => photo.id !== id));
+				setLoading(false);
+				setError(null);
+			})
+			.catch((err) => {
+				setError(err.message);
+				setLoading(false);
+			});
+	};
+
+	useEffect(() => {
+		setLoading(true);
+		fetch(`https://gallery-app-server.vercel.app/photos?_sort=id&_order=${sort}`)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setPhotos(data);
+				setLoading(false);
+				setError(null);
+			})
+			.catch((err) => {
+				setError(err.message);
+				setLoading(false);
+			});
+		//jika search tidak kosong
+		if (search) {
+			//lakukan search pada server dengan url http://localhost:3001/photos?q={search}
+			fetch(`https://gallery-app-server.vercel.app/photos?q=${search}`)
+				.then((res) => {
+					return res.json();
+				})
+				.then((data) => {
+					setPhotos(data);
+					setLoading(false);
+					setError(null);
+				})
+				.catch((err) => {
+					setError(err.message);
+					setLoading(false);
+				});
+		}
+	}, [sort, submited]);
+
+	useEffect(() => {
+		setLoading(true);
+		fetch('https://gallery-app-server.vercel.app/photoss')
+			.then((res) => res.json())
+			.then((res) => {
+				setPhotos(res);
+				setLoading(false);
+			})
+			.catch((err) => {
+				setError(err);
+				setLoading(false);
+			});
+	}, []);
+
+	if (error) return <h1 style={{ width: '100%', textAlign: 'center', marginTop: '20px' }}>Error!</h1>;
+
+	return (
+		<>
+			<div className="container">
+				<div className="options">
+					<select onChange={(e) => setSort(e.target.value)} data-testid="sort" className="form-select" style={{}}>
+						<option value="asc">Ascending</option>
+						<option value="desc">Descending</option>
+					</select>
+					<form
+						onSubmit={(e) => {
+							e.preventDefault();
+							setSubmited(search);
+						}}
+					>
+						<input type="text" data-testid="search" onChange={(e) => setSearch(e.target.value)} className="form-input" />
+						<input type="submit" value="Search" data-testid="submit" className="form-btn" />
+					</form>
+				</div>
+				<div className="content">
+					{loading ? (
+						<h1 style={{ width: '100%', textAlign: 'center', marginTop: '20px' }}>Loading...</h1>
+					) : (
+						photos.map((photo) => {
+							return <Card key={photo.id} photo={photo} deletePhoto={deletePhoto} />;
+						})
+					)}
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Photos;
